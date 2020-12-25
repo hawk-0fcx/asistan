9e60a7c357996ba0cbb39de9bdd00a1538557ff7
hawk.0fcx <67156465+hawk-0fcx@users.noreply.github.com>
2020-12-25 16:21:30 +0300

    Add files via upload

---
+++ install.py
@@ -0,0 +1,9 @@
+import os
+os.system("pip install beautifulsoup4")
+os.system("pip install pycopy-webbrowser")
+os.system("python -m pip install urllib3")
+os.system("pip install hashlib")
+os.system("pip install requests")
+os.system("pip install wikipedia")
+os.system("pip install colorama")
+os.system("pip install feedparser")
